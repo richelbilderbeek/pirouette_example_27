d9b7d3961c98f58468edf26900d4d90171c421b9
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-16 14:38:59 +0100

    Nice script

--- example_27.R
+++ example_27.R
@@ -1,47 +1,88 @@
 #
-# Difference from standard: 
+# Difference from standard:
 # - Uses copy_tree for twin tree
 # - Uses unlinked node-subst
 #
 library(pirouette)
 library(beautier)
+library(testthat)
+library(ggplot2)
 
 # Constants
 is_testing <- is_on_ci()
-example_no <- 17
+example_no <- 27
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
-
-# Create phylogeny
-phylogeny  <- ape::read.tree(
-  text = "(((A:8, B:8):1, C:9):1, ((D:8, E:8):1, F:9):1);"
-)
-
-# Setup pirouette
-pir_params <- create_std_pir_params(
-  folder_name = folder_name
-)
-pir_params$alignment_params$sim_tral_fun <-
-  get_sim_tral_with_uns_nsm_fun(
-    branch_mutation_rate = 0.1,
-    node_mutation_rate = 0.1
-  )
-pir_params$twinning_params$sim_twin_tree_fun <- create_copy_twtr_from_true_fun()
+crown_age <- 10
+n_phylogenies <- 5
 if (is_testing) {
-  pir_params <- shorten_pir_params(pir_params)
+  n_phylogenies <- 2
 }
 
-# Run pirouette
-pir_out <- pir_run(
-  phylogeny,
-  pir_params = pir_params
+# Create simulation function
+sim_dd_tree_fun <- function(crown_age) {
+  extinction_rate <- 0.1
+  n_taxa <- 6
+  n_0 <- 2 # Initial number of species at stem/crown of tree
+  diff <- (log(n_taxa) - log(n_0)) / crown_age
+  speciation_rate <- 3.0 * (diff + extinction_rate)
+  carrying_capacity <- n_taxa # clade-level
+  dd_parameters <- c(speciation_rate, extinction_rate, carrying_capacity)
+  ddmodel <- 1 # linear dependence in speciation rate with parameter K
+  dd_sim_result <- DDD::dd_sim(pars = dd_parameters, age  = crown_age, ddmodel = ddmodel)
+  phylogeny <- dd_sim_result$tes # Only extant species
+  phylogeny
+}
+sim_tree_fun <- pryr::partial(
+  sim_dd_tree_fun,
+  crown_age = crown_age
 )
 
-# Save results
-pir_save(
-  phylogeny = phylogeny,
-  pir_params = pir_params,
-  pir_out = pir_out,
-  folder_name = folder_name
+# Create phylogenies
+phylogenies <- list()
+for (i in seq_len(n_phylogenies)) {
+  set.seed(314 - 1 + i)
+  phylogenies[[i]] <- sim_tree_fun()
+}
+expect_equal(length(phylogenies), n_phylogenies)
+
+# Create pirouette parameter sets
+pir_paramses <- create_std_pir_paramses(n = length(phylogenies))
+for (i in seq_along(pir_paramses)) {
+  pir_params[[i]]$alignment_params$sim_tral_fun <-
+    get_sim_tral_with_uns_nsm_fun(
+      branch_mutation_rate = 0.1,
+      node_mutation_rate = 0.1
+    )
+  pir_params[[i]]$twinning_params$sim_twin_tree_fun <-
+    create_copy_twtr_from_true_fun()
+}
+expect_equal(length(pir_paramses), n_phylogenies)
+if (is_testing) {
+  pir_paramses <- shorten_pir_paramses(pir_paramses)
+}
+
+# Do the runs
+pir_outs <- pir_runs(
+  phylogenies = phylogenies,
+  pir_paramses = pir_paramses
 )
+
+# Plot total runs
+pir_plots(
+  pir_outs = pir_outs
+) + ggsave(filename = paste0("errors_all.png"), width = 7, height = 7)
+
+# Save indiidual runs
+expect_equal(length(pir_paramses), length(pir_outs))
+expect_equal(length(pir_paramses), length(phylogenies))
+for (i in seq_along(pir_outs)) {
+  pir_save(
+    phylogeny = phylogenies[[i]],
+    pir_params = pir_paramses[[i]],
+    pir_out = pir_outs[[i]],
+    folder_name = dirname(pir_paramses[[i]]$alignment_params$fasta_filename)
+  )
+}
+
 
